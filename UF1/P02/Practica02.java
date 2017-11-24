45b8b48a45a73e65bde5606ed8df505f94a2828f
harambeisnotdead <harambeisnotdead@users.noreply.github.com>
2017-11-24 20:21:39 +0100

    Primer commit P02

--- UF1/P02/Practica02.java
+++ UF1/P02/Practica02.java
@@ -1,29 +1,46 @@
 import java.util.Arrays;
-import java.util.concurrent.*;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-class Practica02 {
-    static int[] v = new int[1000];
-    static List<Integer> memo;
+class Practica02v2 {
+    static int[] v = new int[10];
+    static int index = 0;
 
-    static {
-        for (int i=0; i<1000; i++) v[i] = i%6 + 1;
+    static void fillArray() {
+        while (index<10) {
+            synchronized(v) {
+                int r = ((int) (Math.random() * 6) + 1);
+                v[index] = r;
+                System.out.println("[*] " + Thread.currentThread().getName()
+                + " -> ["+index+"] = "+ r + " - " + v[index]);
+                index++;
+            }
+            // try {
+            //     TimeUnit.MILLISECONDS.sleep(100);
+            // } catch (InterruptedException e) {}
+        }
     }
 
-    static void delNotUnique(int i, int f) {
-        for (; i<=f; i++) {
-            for (int j=0; j<memo.size(); j++) {
-                if (v[i] != memo.get(j)) memo.add(v[i]);
-            }
+    static void delRepetidos(int i, int f) {
+        System.out.format("[+]\n");
+        if (i < f) {
+            if (v[i] == v[i+1]) {
+                v[i] = 0;
+                delRepetidos(i+1, f);
+                v[i+1] = 0;
+            } else delRepetidos(i+1, f);
         }
-
     }
 
     public static void main(String[] args) {
-        // System.out.println(Arrays.toString(v));
-        ExecutorService executor = Executors.newFixedThreadPool(4);
-        Runnable task = () -> delNotUnique(0, 250);
-        executor.submit(task);
+        System.out.println(Arrays.toString(v));
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        executor.submit(() -> fillArray());
+        executor.submit(() -> fillArray());
+        executor.submit(() -> delRepetidos(0, 9));
         executor.shutdown();
+        while(!executor.isTerminated()) executor.shutdownNow();
+        if (executor.isTerminated()) System.out.println(Arrays.toString(v));
     }
 }
